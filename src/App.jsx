3f2fd8a9c358d4c4ec3ae62f05c50abfe34f6019
Rizu <91118932+rizzzky78@users.noreply.github.com>
2022-11-06 21:44:36 +0700

    new project yeayy!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Component from "./components/test/Component";
+import CompHeader from "./components/web/CompHeader";
+
+function App() {
+  return (
+    <>
+      <CompHeader />
+      <Component />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/test/Component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Component() {
+  return (
+    <div>This is My Component</div>
+  )
+}
+
+export default Component
